1aec29094e9e0ce5dbd46f58cb665d774c92e414
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-03 13:02:25 -0300

    A classe Position irá representar a posição de uma peça no tabuleiro, como, por exemplo, a posição 'e2' no tabuleiro de xadrez

--- chess-system-java/src/boardgame/Position.java
+++ chess-system-java/src/boardgame/Position.java
@@ -1,4 +1,40 @@
 package boardgame;
+
 public class Position {
-    // ... (Code for representing a position on the board)
+    private int row;
+    private int column;
+
+    // Construtor
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    // Getters e Setters
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    // Método para verificar se duas posições são iguais
+    public boolean equals(Position other) {
+        return this.row == other.row && this.column == other.column;
+    }
+
+    // Método para imprimir a posição
+    @Override
+    public String toString() {
+        return "(" + row + ", " + column + ")";
+    }
 }
