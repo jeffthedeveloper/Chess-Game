bfc308f7a9b9294f70941a7e49fb930d7c4a8373
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-03 13:33:43 -0300

    adicionada uma bifurcação lógica, para a queen que combina os movimentos do bispo e da torre, para verificação de movimentação verticas/horizontais e movimentos diagonais

--- chess-system-java/src/pieces/Queen.java
+++ chess-system-java/src/pieces/Queen.java
@@ -8,6 +8,18 @@ public class Queen extends Piece {
     public boolean isValidMove(int newX, int newY) {
         int deltaX = Math.abs(newX - this.x);
         int deltaY = Math.abs(newY - this.y);
-        return (deltaX == deltaY || this.x == newX || this.y == newY);
+
+        // Movimentos verticais/horizontais
+
+        if (deltaX == 0 || deltaY == 0) {
+            return !isPathBlocked(newX, newY, board);
+        }
+
+        // Movimentos diagonais
+
+        if (deltaX == deltaY) {
+            return !isPathBlocked(newX, newY, board);
+        }
+        return false;
     }
 }
