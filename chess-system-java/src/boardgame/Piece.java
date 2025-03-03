0df8c1ee050f6864b001cef362a7198a0e703365
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-03 13:04:00 -0300

    será a classe base para todas as peças do jogo. Ela possui a posição atual e alguns métodos genéricos, como mover a peça e verificar se ela pode se mover para uma posição específica.

--- chess-system-java/src/boardgame/Piece.java
+++ chess-system-java/src/boardgame/Piece.java
@@ -1,4 +1,29 @@
 package boardgame;
-public class Piece {
-    // ... (Code for representing a generic board and its operations)
+
+public abstract class Piece {
+    protected Position position;
+
+    // Construtor
+    public Piece(Position position) {
+        this.position = position;
+    }
+
+    // Getter e Setter
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    // Método abstrato para verificar se a peça pode se mover para a posição
+    public abstract boolean canMoveTo(Position position);
+
+    // Método abstrato para movimentar a peça (necessário implementar nas classes
+    // filhas)
+    public abstract void move(Position newPosition);
+
+    // Método para representar a peça como string (subclasse deve sobrescrever)
+    public abstract String toString();
 }
