9e7391575a80460b4803719306caa01546958e3b
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-03 13:11:15 -0300

    implementando a lógica de verificação de bloqueio do caminho por alguma outra peça

--- chess-system-java/src/pieces/Bishop.java
+++ chess-system-java/src/pieces/Bishop.java
@@ -9,5 +9,23 @@ public class Bishop extends Piece {
         int deltaX = Math.abs(newX - this.x);
         int deltaY = Math.abs(newY - this.y);
         return (deltaX == deltaY);
+
+        // Verificando se o caminho está bloqueado
+
+        int stepX = (newX > this.x) ? 1 : -1;
+        int stepY = (newY > this.y) ? 1 : -1;
+
+        int tempX = this.x + stepX;
+        int tempY = this.y + stepY;
+
+        while (tempX != newX && tempY != newY) {
+            if (board[tempX][tempY] != null) {
+                return false; // Caminho bloqueado
+            }
+            tempX += stepX;
+            tempY += stepY;
+        }
+        return true;
+
     }
 }
