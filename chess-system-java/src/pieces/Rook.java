a49eb550fa830ebfc933e97258cdeb441f893e72
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-03 13:35:12 -0300

    adicionada funcionalidade de verificação de obstrução do caminho

--- chess-system-java/src/pieces/Rook.java
+++ chess-system-java/src/pieces/Rook.java
@@ -6,6 +6,13 @@ public class Rook extends Piece {
 
     @Override
     public boolean isValidMove(int newX, int newY) {
+
+        if (this.x != newX && this.y != newY) {
+            return false;
+        }
         return (this.x == newX || this.y == newY);
+
+        // Verificar se o caminho está bloqueado
+        return !isPathBlocked(newX, newY, board);
     }
 }
